efc6189826a78e6e27545774134bf2ec7f44d1ff
Anna Hamann <annasoldih@gmail.com>
2021-11-13 20:34:44 -0300

    saturday night fixes

--- src/services/NoMagicStuff.jsx
+++ src/services/NoMagicStuff.jsx
@@ -112,31 +112,6 @@ export const ingredientSrcLink = (ingr) => {
   return linkao;
 };
 
-// localStorage
-// O uso de localStorage é necessário para que as informações não se percam caso a pessoa atualize a página. O correto é usar os valores para iniciar sua store ou seu context.
-
-// No localStorage do navegador:
-
-// a chave mealsToken deve conter a seguinte estrutura:
-// 1
-// a chave cocktailsToken deve conter a seguinte estrutura:
-// 1
-// a chave user deve conter a seguinte estrutura:
-// {
-//     email: email-da-pessoa
-// }
-// a chave doneRecipes deve conter a seguinte estrutura:
-// [{
-//     id: id-da-receita,
-//     type: comida-ou-bebida,
-//     area: area-da-receita-ou-texto-vazio,
-//     category: categoria-da-receita-ou-texto-vazio,
-//     alcoholicOrNot: alcoholic-ou-non-alcoholic-ou-texto-vazio,
-//     name: nome-da-receita,
-//     image: imagem-da-receita,
-//     doneDate: quando-a-receita-foi-concluida,
-//     tags: array-de-tags-da-receita-ou-array-vazio
-// }]
 // a chave favoriteRecipes deve conter a seguinte estrutura:
 // [{
 //     id: id-da-receita,
